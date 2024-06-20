142e58ae761c065f3fbe144079863b178a7e9086
Guilherme Barrinha <guilhermebarrinhams@gmail.com>
2024-06-20 15:33:13 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,19 @@
 package application;
 
-import models.entities.Departament;
+import java.util.Date;
+
+import models.entities.Department;
+import models.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Departament obj = new Departament(1, "Books");
-		System.out.println(obj);
+		Department obj = new Department(1, "Books");
+		
+		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+		
+		System.out.println(seller);
 		
 	}
 
