67594ff85aa54407dd19010652f5878a7fb1a7a0
Guilherme Barrinha <guilhermebarrinhams@gmail.com>
2024-06-25 16:28:30 -0300

    delete implementation department

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -1,15 +1,16 @@
 package application;
 
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
 import models.entities.Department;
-import models.entities.Seller;
 
 public class Program2 {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		
 		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 		
@@ -22,16 +23,24 @@ public class Program2 {
 			System.out.println(obj);
 		}
 	
-		System.out.println("\n=== TEST 4: Department insert =====");
+		/*System.out.println("\n=== TEST 4: Department insert =====");
 		Department newDepartment = new Department(null, "Gui");
 		departmentDao.insert(newDepartment);
-		System.out.println("Inserted! New id = " + newDepartment.getId());
+		System.out.println("Inserted! New id = " + newDepartment.getId());*/
 		
 		System.out.println("\n=== TEST 5: department update =====");
 		department = departmentDao.findById(5);
 		department.setName("Martha Waine");
 		departmentDao.update(department);
 		System.out.println("Update completed");
+		
+		System.out.println("\n=== TEST 6: department delete =====");
+		System.out.println("Enter id for delete test: ");
+		int id = sc.nextInt();
+		departmentDao.deleteById(id);
+		System.out.println("Delete completed");
+
+		sc.close();
 	}
 
 }
